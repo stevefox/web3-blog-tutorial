079e09f77d1b764eedb611afe15927f7c01ebc58
Stephen Fox <social@stevefox.io>
2022-09-24 19:11:26 -0500

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    }, 
+    // polygon: {
+    //  url: "https://polygon-rpc.com/",
+    //  accounts: [process.env.pk]
+    // }
+  }
+};
+
+export default config;
