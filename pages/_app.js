4e48cccb3eee635dba058daa48bbc636e5214097
Stephen Fox <social@stevefox.io>
2022-09-24 21:00:09 -0500

    Add initial app configuration: context (global react state), two svgs from the tutorial, and imports for pages/_app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+/* pages/__app.js */
+import { useState } from 'react'
+import Link from 'next/link'
+import { css } from '@emotion/css'
+import { ethers } from 'ethers'
+import Web3Modal from 'web3modal'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import { AccountContext } from '../context.js'
+import { ownerAddress } from '../config'
+import 'easymde/dist/easymde.min.css'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
